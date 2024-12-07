9872a6ec0d3cdc962214aec19ed17035263222ca
Karnazh1106 <dekhtiar_ak23@nuwm.edu.ua>
2024-12-07 02:58:33 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -161,21 +161,3 @@ public class Test
         obj.DisplayCoefficients();
     }
 }
-﻿using System;
-
-namespace LabWork
-{
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
